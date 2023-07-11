6533d5245cb5b0c3efe9e46f450ccd57d7ddfdef
Ferda Kizilay <ferda.kizilay@yahoo.com>
2023-07-10 19:34:10 -0500

    sp 2

---
+++ src/test/java/com/brite/step_definition/Hooks.java
@@ -0,0 +1,22 @@
+package com.brite.step_definition;
+
+import com.brite.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    @After
+
+    public void tearDownMethod(Scenario scenario){
+
+        if(scenario.isFailed()) {
+
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+
+        Driver.closeDriver();
+    }
+}
