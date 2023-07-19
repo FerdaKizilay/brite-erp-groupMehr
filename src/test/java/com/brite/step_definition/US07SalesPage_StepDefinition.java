17d3bb245a2b8729a22b5705c61c7298dd33e730
Ruth <berakiruth@gmail.com>
2023-07-19 08:53:36 -0500

    US07, US09

---
+++ src/test/java/com/brite/step_definition/US07SalesPage_StepDefinition.java
@@ -0,0 +1,10 @@
+package com.brite.step_definition;
+
+import com.brite.pages.LoginPage;
+import com.brite.pages.Sales_Page;
+
+public class US07SalesPage_StepDefinition {
+    LoginPage loginPage=new LoginPage();
+    Sales_Page salesPage=new Sales_Page();
+
+}
