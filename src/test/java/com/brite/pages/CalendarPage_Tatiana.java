784be4639e1b2e65f2ca9c5067bd130a90006acb
Tatiana_Chise <ocnovanutanya@yahoo.com>
2023-07-19 00:45:56 -0500

    trying hard to solve

---
+++ src/test/java/com/brite/pages/CalendarPage_Tatiana.java
@@ -0,0 +1,55 @@
+package com.brite.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.brite.utilities.Driver;
+
+
+    public class CalendarPage_Tatiana {
+
+        public CalendarPage_Tatiana(){
+            PageFactory.initElements(Driver.getDriver(),this);
+        }
+
+        @FindBy(xpath = "//span[@class='oe_topbar_name']")
+        public WebElement accountName;
+
+        @FindBy(xpath = "//button[.='Day']")
+        public WebElement dayButton;
+
+        @FindBy(xpath = "//table//table//th[2]")
+        public WebElement dayHeader;
+
+        @FindBy(xpath = "(//td[@class='fc-axis fc-widget-content'])[2]")
+        public WebElement timeColumn;
+
+
+        @FindBy(xpath = "//span[normalize-space(text())='Calendar']")
+        public WebElement calendarModule;
+
+
+        @FindBy(xpath = "//div[@class='fc-row fc-widget-header']")
+        public WebElement daysOfTheWeek;
+
+        @FindBy(xpath = "//button[.='Week']")
+        public WebElement weekModule;
+
+
+        @FindBy(xpath="//thead[@class='fc-head']")
+        public WebElement daysOfTheMonth;
+
+        @FindBy(xpath = "//tbody[@class='fc-body']")
+        public WebElement datesOfTheMonth;
+
+
+
+
+
+
+
+
+
+    }
+
+
