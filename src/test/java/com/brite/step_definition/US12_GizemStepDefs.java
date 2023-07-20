1da903582f96f222135998fdb2e9d19fafdad11a
gizemular <gizemkayaular@gmail.com>
2023-07-19 22:13:02 -0400

    created US12_GizemStepDefs.java

---
+++ src/test/java/com/brite/step_definition/US12_GizemStepDefs.java
@@ -0,0 +1,18 @@
+package com.brite.step_definition;
+
+import com.brite.pages.RepairsPage;
+import io.cucumber.java.en.Then;
+
+public class US12_GizemStepDefs {
+
+    RepairsPage repairsPage = new RepairsPage();
+
+    @Then("Users can select all the repair orders by clicking the top checkbox.")
+    public void users_can_select_all_the_repair_orders_by_clicking_the_top_checkbox() {
+
+        repairsPage.checkBox.click();
+        repairsPage.searchBox.isSelected();
+
+
+    }
+}
