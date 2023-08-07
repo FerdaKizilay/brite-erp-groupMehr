79fc3521240d92e3030353bb222e14f5df7e623c
Tatiana_Chise <ocnovanutanya@yahoo.com>
2023-08-07 03:59:25 -0500

    updated some methods in step definition

--- src/test/java/com/brite/pages/CalendarPage_Tatiana.java
+++ src/test/java/com/brite/pages/CalendarPage_Tatiana.java
@@ -5,8 +5,10 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.brite.utilities.Driver;
 
+import java.util.List;
 
-    public class CalendarPage_Tatiana {
+
+public class CalendarPage_Tatiana {
 
         public CalendarPage_Tatiana(){
             PageFactory.initElements(Driver.getDriver(),this);
@@ -22,30 +24,37 @@ import com.brite.utilities.Driver;
         @FindBy(xpath = "(//table//table//th[2])")
         public WebElement dayHeader;
 
-        @FindBy(xpath = "((//td[@class='fc-axis fc-widget-content'])[2])")
+        @FindBy(xpath = "(//table)[6]//tr")
         public WebElement timeColumn;
+
+        @FindBy(xpath="(//table)[6]//td[.='0:00']")
+        public WebElement timeStartPoint;
+
+        @FindBy(xpath="(//table)[6]//td[.='23:00']")
+        public WebElement timeEndPoint;
+
+
 
 
         @FindBy(xpath = "//span[normalize-space(text())='Calendar']")
         public WebElement calendarModule;
 
 
-        @FindBy(xpath = "//div[@class='fc-row fc-widget-header']")
-        public WebElement daysOfTheWeek;
+        @FindBy(xpath = "(//table)[2]//th")
+        public List<WebElement> daysOfTheWeek;
 
         @FindBy(xpath = "//button[.='Week']")
         public WebElement weekModule;
 
 
-        @FindBy(xpath="//thead[@class='fc-head']")
-        public WebElement daysOfTheWeekInMonth;
-
-       @FindBy(xpath = "//div[@class='fc-day-grid fc-unselectable']")
-       // @FindBy(xpath = "//tbody[@class='fc-body']")
-        public WebElement datesOfTheMonthTable;
+       @FindBy(xpath = "(//table)[1]//td[@class='fc-week-number']/span")
+       public List<WebElement> numberOfRowsInCalendarTable;
 
         @FindBy(xpath = "//button[.='Month']")
         public WebElement monthButton;
+
+        @FindBy(xpath = "(//table)[2]//th")
+        public List<WebElement> monthlyDaysOfTheWeek;
 
         @FindBy(xpath = "//table//tbody//tr[19]//td[2]")
         public WebElement event;
